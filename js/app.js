5a4a70470907ac1bf91b107c009ae5dfc4ca7eb3
Michael <Michaelquiban@Outlook.com>
2016-12-05 15:45:24 -0800

    Attempted to use data

--- js/app.js
+++ js/app.js
@@ -3,15 +3,6 @@
 $( document ).ready(function() {
     console.log( "Ready!" );
 
-    $("#search-form").submit(function(e) {
-		e.preventDefault();
-		var search = $( "#event-type option:selected" ).text();
-		console.log(search + " has been searched for...");
-		console.log("Getting results...");
-		getMeetup(search);
-		console.log("Results retrieved");
-	});
-
 	function getMeetup(search) {
 		$.ajax({
 			//API Server
@@ -22,12 +13,29 @@ $( document ).ready(function() {
 				key:'d357b7d536712c642322587b5f4719',
 				text: search,
 			},
-			success: function(data){
+			success: function(data) {
 				console.log(data);
+				var results = data.items;
+				var resultsLength = data.length;
+				for (i=0; i < resultsLength; i++) {
+					console.log(resultsLength + "Hello World!");
+				}
 			},
-			error: function(error){
+			error: function(error) {
 				console.log(error);
-			} 
+			}
 		})
 	};
+
+	//Submit request performed here...
+	  $("#search-form").submit(function(e) {
+		e.preventDefault();
+		//Grab the dropdown search query and store it in a variable.
+		var search = $( "#event-type option:selected" ).text();
+		//Debug information.
+		console.log(search + " has been searched for...");
+		console.log("Getting results...");
+		getMeetup(search);
+		console.log("Results retrieved...");
+	});
 });
